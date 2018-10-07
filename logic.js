cfec6a6203df602e0862b92252bdbd549a9c4048
johnbxu <johnbxu@gmail.com>
2018-10-07 00:52:48 +0000

    tidied up some garbage comments

--- logic.js
+++ logic.js
@@ -395,59 +395,6 @@ $(function() {
   };
 
 
-  // console.log(player2);
-
-  // player1.checkHit(player2, [3, 5]);
-  // player1.checkHit(player2, [3, 6]);
-  // player1.checkHit(player2, [3, 7]);
-  // player1.checkHit(player2, [4, 5]);
-  // player1.checkHit(player2, [5, 5]);
-  // player1.checkHit(player2, [6, 5]);
-  // player1.checkHit(player2, [7, 5]);
-  // player1.checkHit(player2, [8, 5]);
-  // player1.checkHit(player2, player1.randomHit());
-
-  // console.log('------------player 1 enemy board--------');
-  // console.log(player1.enemyBoard);
-  // console.log('----------------------------------------');
-
-  // console.log('------------player 2 board--------------');
-  // console.log(player2.board);
-  // console.log('----------------------------------------');
-  // console.log('------------player 2 ships--------------');
-  // console.log(player2.ships);
-  // console.log('----------------------------------------');
-
-
-  // console.log(player1.ships.Battleship.coordinates);
-  // console.log(player1.ships[0].coords);
-
-
-  /* Implementation of console inputs  - to do later
-  // const initialize = readline.createInterface({
-  //   input: process.stdin,
-  //   output: process.stdout,
-  // });
-
-  // initialize.question('Please place the carrier and indicate
-  // which direction to place it [up, down, left, right]', (coord)
-  // => {
-  //   player1.placeShip(5, coord[0], coord[1]);
-  //   console.log(coord[0]);
-  //   console.log(coord[1]);
-  //   player1.drawBoard();
-  //   initialize.close();
-  // });
-  */
-
-
-  // execution
-  // player1.initEmptyBoard('self', 10);
-  // player1.initEmptyBoard('enemy', 10);
-  // player2.initEmptyBoard('self', 10);
-
-
-
   const hunt = {
     hunt: false,
     huntStage: 0,
